5b20388fa211c284ab94c438b364e6b2785a0099
Stud Horse <lalalatim@yahoo.com.tw>
2021-07-03 17:46:04 +0800

    fixed frontend

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const sendHtml = () => {
     const input = document.querySelector(".md");
-    const html = document.querySelector(".htmlText");
     const text = input.value;
-    fetch()
+    fetch('https://markdown-converter-api.herokuapp.com/convert', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'type': 'html', 'value': text })
+    })
+        .then(fetch('https://markdown-converter-api.herokuapp.com/download', {
+            method: 'get',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 'type': 'html', 'value': text })
+        }))
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+const sendPdf = () => {
+    const input = document.querySelector(".md");
+    const text = input.value;
+    fetch('https://markdown-converter-api.herokuapp.com/convert', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'type': 'pdf', 'value': text })
+    })
+        .then(fetch('https://markdown-converter-api.herokuapp.com/download', {
+            method: 'get',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 'type': 'pdf', 'value': text })
+        }))
+        .catch(err => {
+            console.log(err);
+        });
 }

d1d73aba55e4fe8cb4c01a8cb854751c37f7cb3f
Stud Horse <lalalatim@yahoo.com.tw>
2021-07-03 22:05:34 +0800

    added cors prefix

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const sendHtml = () => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 'type': 'html', 'value': text })
     })
-        .then(fetch('https://markdown-converter-api.herokuapp.com/downloadHtml'))
+        .then(fetch('https://lit-waters-04527.herokuapp.com/https://markdown-converter-api.herokuapp.com/downloadHtml'))
+        .then(console.log('downloaded'))
         .catch(err => {
             console.log(err);
         });
@@ -20,7 +21,7 @@ const sendPdf = () => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 'type': 'pdf', 'value': text })
     })
-        .then(fetch('https://markdown-converter-api.herokuapp.com/downloadPdf'))
+        .then(fetch('https://lit-waters-04527.herokuapp.com/https://markdown-converter-api.herokuapp.com/downloadPdf'))
         .catch(err => {
             console.log(err);
         });
